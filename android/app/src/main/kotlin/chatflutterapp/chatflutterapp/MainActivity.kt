9fae5227982efa7ee949f13a4522fc6658e91f20
devenderbutani21 <devenderbutani21@gmail.com>
2020-09-13 19:11:58 -0400

    Basic Structure

---
+++ android/app/src/main/kotlin/chatflutterapp/chatflutterapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package chatflutterapp.chatflutterapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat App',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
